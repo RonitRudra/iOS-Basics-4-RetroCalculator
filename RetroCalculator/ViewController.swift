26facf2b550bcd802daa5bf02b36b2193cd6be3e
Ronit Rudra <ronit.rudra1995@gmail.com>
2017-07-29 14:45:22 -0500

    Added IBAction to Buttons

--- RetroCalculator/ViewController.swift
+++ RetroCalculator/ViewController.swift
@@ -7,19 +7,41 @@
 //
 
 import UIKit
+// For Audio
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    // create variable of type AVAudioPlayer
+    var buttonSound : AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        // get path of btn of type wav
+        let path = Bundle.main.path(forResource: "btn", ofType: "wav")
+        // save it as URL and ! means that path must have value
+        let soundURL = URL(fileURLWithPath : path!)
+        
+        
+        // Do this with URL
+        do{
+            try buttonSound = AVAudioPlayer(contentsOf : soundURL)
+            buttonSound.prepareToPlay()
+        }
+        // if it fails do this otherwis app might crash if URL empty
+        catch let err as NSError{
+            print(err.debugDescription )
+        }
+    
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func numberPressed(sender : UIButton ){
+        
     }
-
+    
+    func playSound(){
+        
+    }
 
 }
 
